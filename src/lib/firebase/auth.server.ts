d08beac2a05a926e070a0538e698bc116d60890b
Hwi Ahn <byron1st@icloud.com>
2023-01-22 19:12:08 +0900

    fix commonjs import

--- src/lib/firebase/auth.server.ts
+++ src/lib/firebase/auth.server.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { initializeApp } from 'firebase-admin';
+import firebaseAdmin from 'firebase-admin';
 import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import type { App } from 'firebase-admin/app';
@@ -9,7 +9,7 @@ let app: App;
 if (dev) {
 	// @ts-ignore
 	if (!global.__firebaseAdminApp) {
-		app = initializeApp({ projectId: env.GCP_PROJECT_ID });
+		app = firebaseAdmin.initializeApp({ projectId: env.GCP_PROJECT_ID });
 		// @ts-ignore
 		global.__firebaseAdminApp = app;
 	}
@@ -17,7 +17,7 @@ if (dev) {
 	// @ts-ignore
 	app = global.__firebaseAdminApp;
 } else {
-	app = initializeApp({ projectId: env.GCP_PROJECT_ID });
+	app = firebaseAdmin.initializeApp({ projectId: env.GCP_PROJECT_ID });
 }
 
 export default app;
